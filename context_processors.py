7c5a81375ea8852379d145aa1cebc4fb8cf33aa4
Grant Callaghan <grant.callagha@gmail.com>
2012-01-11 19:16:39 -0600

    Added basic template

---
+++ context_processors.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+
+def media(request):
+    '''
+    Adds media related context variables to the template context
+    '''
+    return {'320_MEDIA_URL':settings.320_MEDIA_URL}
